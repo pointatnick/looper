d0d33a7f67e3e4c88cc52a77ed51284f9992bfe0
pointatnick <nicholashma@gmail.com>
2017-05-30 22:46:39 -0700

    Video now loops

--- app/src/main/java/pointatnick/looper/MainActivity.java
+++ app/src/main/java/pointatnick/looper/MainActivity.java
@@ -2,6 +2,7 @@ package pointatnick.looper;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -9,19 +10,29 @@ import android.widget.VideoView;
 
 public class MainActivity extends Activity {
 
+  private VideoView vv;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    // attach VideoView to layout
+    vv = (VideoView) findViewById(R.id.video_view);
+
+    // set video to loop
+    vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+      @Override
+      public void onPrepared(MediaPlayer mp) {
+        mp.setLooping(true);
+      }
+    });
   }
 
   @Override
   public void onWindowFocusChanged(boolean hasFocus) {
     super.onWindowFocusChanged(hasFocus);
     if (hasFocus) {
-      // create VideoView
-      VideoView vv = (VideoView) findViewById(R.id.video_view);
-
       // set VideoView to be fullscreen
       vv.setSystemUiVisibility(
               View.SYSTEM_UI_FLAG_LAYOUT_STABLE
