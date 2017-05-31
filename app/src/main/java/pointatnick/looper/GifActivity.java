60704db30356f17a262632d3bb0853f10c34143c
pointatnick <nicholashma@gmail.com>
2017-05-30 22:39:20 -0700

    Set GIF to be fullscreen as well

--- app/src/main/java/pointatnick/looper/GifActivity.java
+++ app/src/main/java/pointatnick/looper/GifActivity.java
@@ -2,8 +2,13 @@ package pointatnick.looper;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import pl.droidsonroids.gif.GifImageView;
 
 public class GifActivity extends Activity {
+
+  private GifImageView giv;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,16 @@ public class GifActivity extends Activity {
   public void onWindowFocusChanged(boolean hasFocus) {
     super.onWindowFocusChanged(hasFocus);
     if (hasFocus) {
+
+      // set gif to be fullscreen
+      giv.setSystemUiVisibility(
+              View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                      | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                      | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                      | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                      | View.SYSTEM_UI_FLAG_FULLSCREEN
+                      | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
       // start timer for 5 seconds, then end activity
 
     }
